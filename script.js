9faa1cef54e3ead1f258b78b5f5e6cdd7352e1c1
Rene Silva <renivaldo.lopes33@gmail.com>
2020-08-09 20:15:15 -0300

    Criada a funçao igual que resolve a operaçao digitada com a funçao eval()

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function inserir(elemento) {
     var visor = document.getElementById('visor');
     visor.value = visor.value + elemento.innerHTML;
     console.log(visor.value);
+}
 
+function igual(){
+    var valorDigitado = document.getElementById('visor').value;
+    document.getElementById('visor').value = eval(valorDigitado);
 }
