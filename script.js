3a985481a77d84bf46376977bc4e0ab507e01e2c
Rene Silva <renivaldo.lopes33@gmail.com>
2020-08-10 20:57:04 -0300

    Foi criada uma funçao para apagar todos os valores digitados e outra apagar apagar apenas o ultimo digito. Incluido estrutura para prevenir erro quando funcao igual era chamada vazia

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 function inserir(elemento) {
     var visor = document.getElementById('visor');
     visor.value = visor.value + elemento.innerHTML;
-    console.log(visor.value);
+    
 }
 
 function igual(){
+
     var valorDigitado = document.getElementById('visor').value;
-    document.getElementById('visor').value = eval(valorDigitado);
+    
+    if (!valorDigitado ) {
+        document.getElementById('visor').value = "";
+
+    } else {
+        document.getElementById('visor').value = eval(valorDigitado);
+    }
+}
+
+function limpar(){
+    document.getElementById('visor').value = "";
+}
+
+function apagarUltimo(){
+    var valorDigitado = document.getElementById('visor').value;
+    document.getElementById('visor').value = valorDigitado.slice(0, -1);
 }
